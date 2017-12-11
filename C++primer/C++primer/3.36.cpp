2791ea00179fbaa50dcd81b601a895968f19a897
willleepang <pangwilllee@hotmail.com>
2017-12-11 22:39:18 +0800

    增加了几个测试文件
    3.36

--- C++primer/C++primer/3.36.cpp
+++ C++primer/C++primer/3.36.cpp
@@ -20,4 +20,21 @@ int main(){
         }
         return true;
     }
+
+    int main(){
+        int arr1[3] = { 0, 1, 2 }; 
+        int arr2[3] = { 0, 2, 4 };
+        if (compare(begin(arr1), end(arr1), begin(arr2), end(arr2)))
+            cout << "The two arrays are equal." << endl;
+        else
+            cout << "The two arrays are not equal." << endl;
+        cout << "==========" << endl;
+
+        vector<int> vec1 = { 0, 1, 2 };
+        vector<int> vec2 = { 0, 1, 2 };
+        if (vec1 == vec2)
+            cout << "The two vectors are equal." << endl;
+        else
+            cout << "The two vectors are not equal." << endl;
+    }
 }
